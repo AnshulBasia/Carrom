dc027bf8e5e29a5b4a57cc79618e22752e2d871f
Anshul Basia <ee3140502@iitd.ac.in>
2015-06-13 12:21:38 +0530

    Create render_engine.cpp

---
+++ render_engine.cpp
@@ -0,0 +1,36 @@
+#include "render_engine.h"
+#include "renderstate.h"
+#include <GL/glut.h>
+
+
+void render_engine::init() 
+{
+  glClearColor(0.0, 0.0, 0.0, 0.0);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+  
+  glEnable (GL_LINE_SMOOTH);
+  glHint (GL_LINE_SMOOTH_HINT, GL_DONT_CARE);
+  glLineWidth (1.5);
+}
+
+void render_engine::reshape(int w, int h)
+{
+  glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+}
+
+
+void render_engine::keyboard(unsigned char key, int x, int y)
+{
+  switch (key) {
+    case 27:
+      exit(0);
+      break;
+  }
+}
+
